ca447ee9642fb4c1d6079efbc649efa580fd696c
evgenymarkov <evgenymarkov@yandex.com>
2024-04-28 00:58:16 +0200

    Add tag to example

--- examples/basic/internal/api/api.go
+++ examples/basic/internal/api/api.go
@@ -7,6 +7,11 @@ import (
 	"github.com/evgenymarkov/oasis/openapi3"
 )
 
+var (
+	greetingsDocs = openapi3.NewExternalDocumentation("https://wikipedia.org/wiki/Hello,_world!").
+		SetDescription("Free online encyclopedia, created and edited by volunteers")
+)
+
 func NewAPI(mux *http.ServeMux) *oasis.API {
 	api := oasis.NewAPI(
 		mux,
@@ -16,7 +21,12 @@ func NewAPI(mux *http.ServeMux) *oasis.API {
 			SetSwaggerUITitle("API Docs"),
 		openapi3.NewDocument().
 			SetTitle("Greeting API").
-			SetVersion("1.0.0"),
+			SetVersion("1.0.0").
+			SetTags(
+				openapi3.NewTag("greetings").
+					SetDescription("Greetings operations").
+					SetExternalDocs(greetingsDocs),
+			),
 	)
 
 	api.Get(
