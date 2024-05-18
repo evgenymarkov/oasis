03f4acf6311157f313bde475fd36b9e68cb7a4d2
evgenymarkov <evgenymarkov@yandex.com>
2024-05-18 18:05:59 +0200

    Rename arguments

--- openapi3/common_test.go
+++ openapi3/common_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func assertObjectSerialization(t *testing.T, expected map[string]any, document any) {
+func assertObjectSerialization(t *testing.T, wantMap map[string]any, gotObject any) {
 	t.Helper()
 
-	wantBytes, wantErr := json.Marshal(expected)
-	gotBytes, gotErr := json.Marshal(document)
+	wantBytes, wantErr := json.Marshal(wantMap)
+	gotBytes, gotErr := json.Marshal(gotObject)
 
 	require.NoError(t, wantErr)
 	require.NoError(t, gotErr)
